847f6424050ae4b163bfa276b2b4bcaa1a9e9c31
league <league@wts20.attlocal.net>
2018-03-12 19:56:38 -0700

    Horoscope Shiny Object Years alive

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,16 +5,21 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+String Shiny=JOptionPane.showInputDialog("How many shiny objects do you want?");
 		// 3. Play the sound that many times
-
+int shiny=Integer.parseInt(Shiny);
+for (int i = 0; i < shiny; i++) {
+	playMisterZee();
+}
 		// 1. Call the method below
+
 
 	}
 
---
+++ src/modulo/YearsAlive.java
@@ -0,0 +1,15 @@
+package modulo;
+
+public class YearsAlive  {
+	public static void main(String[] args) {
+		for (int i = 2006; i <= 2018; i++) {
+			
+				
+			
+		
+		System.out.println("In "+i+", I was " + ( i-2006 )+ " years old.");
+		
+		}
+	}
+}
+
